914a14fbdd01208727ea0a61798348f6a9a35653
Imad-t <imadeddineterraf@gmail.com>
2023-12-08 16:44:38 +0100

    changed menu button color in darkmode

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 document.getElementById('darkmode').addEventListener('change', (event) => {
     const root = document.documentElement;
+    //to change the menu button color in darkmode
+    const menu = document.querySelector('.menu');
+    const openMenu = document.querySelector('.open .menu');
+
     //switch the css color variables to get darkmode
     if (event.target.checked) {
         root.style.setProperty('--bg-color', '#223344');
         root.style.setProperty('--txt-color', '#F2F9FE');
         root.style.setProperty('--toggle-bg', '#111729');
+        menu.classList.add('dark-mode-filter');
+        openMenu.classList.add('dark-mode-filter');
     } else {
         root.style.setProperty('--bg-color', ''); 
         root.style.setProperty('--txt-color', '');
         root.style.setProperty('--toggle-bg', '');
+        menu.classList.remove('dark-mode-filter');
+        openMenu.classList.remove('dark-mode-filter');
     }
 
      //change the color of the icon
@@ -34,3 +42,4 @@ function showMenu() {
 
     nav.classList.toggle('open');
 }
+//change menu button color in darkmode
